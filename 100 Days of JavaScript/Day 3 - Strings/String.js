67d808ced64a4a4d8862034a380b1ecde097e898
ahsan-chy <ahsan.chy.iqbal@gmail.com>
2022-12-08 11:29:01 +0500

    DAy 3 - String

---
+++ 100 Days of JavaScript/Day 3 - Strings/String.js
@@ -0,0 +1,20 @@
+let username= "Anonymous Boy";
+
+console.log(username)               // Anonymous Boy
+console.log(username.length)        //16
+
+console.log(username.toLowerCase());  //anonymous boy
+console.log(username.toUpperCase());  //ANONYMOUS BOY
+
+console.log(username.replace('Anonymous', "Ahsan"));  //Ahsan Boy
+
+console.log(username.slice(3,9));    //nymous
+
+console.log('Ahsan'.split(''));      // [ 'A', 'h', 's', 'a', 'n' ]
+console.log(username.split(' '));    //[ 'Anonymous', 'Boy' ]
+
+let Name = "Ahsan";
+let age = 23;
+
+console.log(Name.concat(" Age is ",age)) //Ahsan Age is 23
+
